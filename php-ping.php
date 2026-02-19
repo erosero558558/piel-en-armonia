a7ca5b2a42f3d0924c9f9f881ed769b7e6c5866d
Tu Nombre <tu-email@ejemplo.com>
2026-02-19 10:13:26 -0500

    Remove temporary production diagnostic endpoints

--- php-ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode([
-    'ok' => true,
-    'service' => 'php-ping',
-    'timestamp' => gmdate('c')
-], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
