c302e3ea9448ca902148fa1002ce07c7812724fd
Tu Nombre <tu-email@ejemplo.com>
2026-02-21 07:24:49 -0500

    Bump asset versions to phase9 for cache invalidation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pielarmonia-v6-20260221-phase8-fullcssfix1';
+const CACHE_NAME = 'pielarmonia-v7-20260221-phase9-chatbridge1';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -7,8 +7,8 @@ const STATIC_ASSETS = [
     '/admin.css',
     '/admin.js',
     '/styles-deferred.css?v=ui-20260221-deferred18-fullcssfix1',
-    '/bootstrap-inline-engine.js?v=figo-bootstrap-20260221-phase8-fullcssfix1',
-    '/script.js?v=figo-20260221-phase8-fullcssfix1',
+    '/bootstrap-inline-engine.js?v=figo-bootstrap-20260221-phase9-chatbridge1',
+    '/script.js?v=figo-20260221-phase9-chatbridge1',
     '/images/optimized/hero-woman.jpg',
     '/favicon.ico',
     '/manifest.json',
